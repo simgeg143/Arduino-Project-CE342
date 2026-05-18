e9c0e55f595d98e0efffe8c7be57a1f160324e2f
Sezen Çetinkaya <152431280+SezenCetinkaya@users.noreply.github.com>
2026-05-18 15:14:24 +0300

    Update sketch_may17a.ino

--- sketch_may17a/sketch_may17a.ino
+++ sketch_may17a/sketch_may17a.ino
@@ -7,7 +7,6 @@ int lm35Pin = A4;
 int potPin = A8;
 int ldr = A11;
 
-int led = 8;
 int buzzer = 5;
 int relay = 6;
 
@@ -19,10 +18,11 @@ int ePin = 26;
 int fPin = 27;
 int gPin = 28;
 int hPin = 29;
+
 int GND1 = 33;
 int GND2 = 34;
 int GND3 = 35;
-int GND4 = 36;
+int GND4 = 36;  // rightmost digit
 
 LiquidCrystal lcd(36, 37, 26, 27, 28, 29);
 
@@ -31,29 +31,14 @@ int lm35Value = 0;
 int potVal = 0;
 int ldrValue = 0;
 
+int displayMode = 0;
+char currentChar = ' '; 
+
 void setup(){
-
-  pinMode(led,OUTPUT);
-  pinMode(buzzer,OUTPUT);
+  pinMode(buzzer, OUTPUT);
   pinMode(relay, OUTPUT);
-
-  pinMode(aPin,OUTPUT);
-  pinMode(bPin,OUTPUT);
-  pinMode(cPin,OUTPUT);
-  pinMode(dPin,OUTPUT);
-  pinMode(ePin,OUTPUT);
-  pinMode(fPin,OUTPUT);
-  pinMode(gPin,OUTPUT);
-  pinMode(hPin,OUTPUT);
-  pinMode(GND1,OUTPUT);
-  pinMode(GND2,OUTPUT);
-  pinMode(GND3,OUTPUT);
-  pinMode(GND4,OUTPUT);
-
-  digitalWrite(GND1, HIGH);
-  digitalWrite(GND2, LOW);
-  digitalWrite(GND3, LOW);
-  digitalWrite(GND4, LOW);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(relay, LOW);
 
   pinMode(LcdRWPin, OUTPUT);
   digitalWrite(LcdRWPin, LOW);
@@ -61,77 +46,122 @@ void setup(){
   pinMode(BackLight, OUTPUT);
   digitalWrite(BackLight, HIGH);
 
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("CE342 Project");
+  delay(1500);
+  lcd.clear();
+}
 
+void updateLCD(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp, 1);
+  lcd.print(" C");
+  
+  lcd.setCursor(0, 1);
+  if(displayMode == 0){
+    lcd.print("LDR: ");
+    lcd.print(ldrValue);
+  } else {
+    lcd.print("Pot: ");
+    lcd.print(potVal);
+  }
+  displayMode = 1 - displayMode;
+}
+
+void updateSegment(){
+  pinMode(aPin, OUTPUT);
+  pinMode(bPin, OUTPUT);
+  pinMode(cPin, OUTPUT);
+  pinMode(dPin, OUTPUT);
+  pinMode(ePin, OUTPUT);
+  pinMode(fPin, OUTPUT);
+  pinMode(gPin, OUTPUT);
+  pinMode(hPin, OUTPUT);
+  
+  pinMode(GND1, OUTPUT);
+  pinMode(GND2, OUTPUT);
+  pinMode(GND3, OUTPUT);
+  pinMode(GND4, OUTPUT);
+  
+  digitalWrite(GND1, HIGH);
+  digitalWrite(GND2, LOW);
+  digitalWrite(GND3, LOW);
+  digitalWrite(GND4, LOW);   
+  
+  digitalWrite(LcdRWPin, LOW);
+  
+  if(currentChar == 'A'){
+    showA();
+  } else if(currentChar == 'C'){
+    showC();
+  } else {
+    clearSegments();
+  }
 }
 
 void loop(){
+  analogRead(potPin); delay(5);
+  potVal = analogRead(potPin);
+  analogRead(lm35Pin); delay(5);
+  lm35Value = analogRead(lm35Pin);
+  analogRead(ldr); delay(5);
+  ldrValue = analogRead(ldr);
 
-potVal = analogRead(potPin);
-lm35Value = analogRead(lm35Pin);
-ldrValue = analogRead(ldr);
+  temp = (lm35Value * 5.0 * 100.0) / 1023.0;
 
-temp = (lm35Value*5.0*100.0)/1023.0;
-
-lcd.clear();
-lcd.setCursor(0,0);
-lcd.print("Temp:" );
-lcd.print(temp);
-lcd.print(" C");
-
-if(temp < 20){
-  showA();
-
-  digitalWrite(buzzer,LOW);
-  digitalWrite(led,LOW);
-
-}else{
-
-  showC();
-
-  if (potVal > 600) {
+  if(temp < 20){
+    currentChar = 'A';
+    digitalWrite(buzzer, LOW);
+  } else {
+    currentChar = 'C';
+    if(potVal > 600){
       digitalWrite(buzzer, HIGH);
     } else {
       digitalWrite(buzzer, LOW);
     }
   }
 
-lcd.setCursor(0,1);
-lcd.print("LDR:");
-lcd.print(ldrValue);
-lcd.print(" P:");
-lcd.print(potVal);
+  if(ldrValue > 700){
+    digitalWrite(relay, HIGH);
+  } else {
+    digitalWrite(relay, LOW);
+  }
 
-if(ldrValue > 700){
-  digitalWrite(relay, HIGH);
-}else{
-  digitalWrite(relay, LOW);
+  updateLCD();
+  
+ updateSegment();
+
+  delay(1500);
 }
 
-  delay(500);
-
+void clearSegments(){
+  digitalWrite(aPin, LOW);
+  digitalWrite(bPin, LOW);
+  digitalWrite(cPin, LOW);
+  digitalWrite(dPin, LOW);
+  digitalWrite(ePin, LOW);
+  digitalWrite(fPin, LOW);
+  digitalWrite(gPin, LOW);
+  digitalWrite(hPin, LOW);
 }
 
-void showA() {
-
+void showA(){
+  clearSegments();
   digitalWrite(aPin, HIGH);
   digitalWrite(bPin, HIGH);
   digitalWrite(cPin, HIGH);
-  digitalWrite(dPin, LOW);
   digitalWrite(ePin, HIGH);
   digitalWrite(fPin, HIGH);
   digitalWrite(gPin, HIGH);
-  digitalWrite(hPin, LOW);
 }
 
-void showC() {
-
+void showC(){
+  clearSegments();
   digitalWrite(aPin, HIGH);
-  digitalWrite(bPin, LOW);
-  digitalWrite(cPin, LOW);
   digitalWrite(dPin, HIGH);
   digitalWrite(ePin, HIGH);
   digitalWrite(fPin, HIGH);
-  digitalWrite(gPin, LOW);
-  digitalWrite(hPin, LOW);
 }
